6fd59f69d20261e3bb587d38ad29af9d5afdd4c2
Andrey <yyootttaa@gmail.com>
2024-04-04 14:25:27 +0300

    smth

--- src/helper_scripts/create_sim_plots.py
+++ src/helper_scripts/create_sim_plots.py
@@ -4,13 +4,13 @@ import os
 import matplotlib.pyplot as plt
 
 
-log_name = "group_exploration2024-02-27_19-07-49.json"
+log_name = "group_exploration2024-03-18_12-32-27.json"
 log_file = f"/home/leev/simulation_logs/{log_name}"
 
-log_name2 = "modified_pso2024-02-28_11-22-01.json"
+log_name2 = "modified_pso2024-03-18_12-52-28.json"
 log_file2 = f"/home/leev/simulation_logs/{log_name2}"
 
-log_name3 = "frontier_pso2024-02-28_11-34-57.json"
+log_name3 = "frontier_pso2024-03-18_12-36-29.json"
 log_file3 = f"/home/leev/simulation_logs/{log_name3}"
 
 
@@ -27,6 +27,8 @@ def main():
     with open(f"{log_file3}", "r+") as log_fh:
         loaded_dict3 = json.load(log_fh)
 
+
+
     percentage_arr = []
     simulation_seconds_arr = []
     average_covered_path_arr = []
@@ -39,6 +41,7 @@ def main():
     percentage = np.array(percentage_arr)
     simulation_seconds = np.array(simulation_seconds_arr)
     average_covered_path = np.array(average_covered_path_arr)
+
 
     percentage_arr2 = []
     simulation_seconds_arr2 = []
@@ -53,6 +56,7 @@ def main():
     simulation_seconds2 = np.array(simulation_seconds_arr2)
     average_covered_path2 = np.array(average_covered_path_arr2)
 
+
     percentage_arr3 = []
     simulation_seconds_arr3 = []
     average_covered_path_arr3 = []
@@ -66,15 +70,20 @@ def main():
     simulation_seconds3 = np.array(simulation_seconds_arr3)
     average_covered_path3 = np.array(average_covered_path_arr3)
     
-    
+
+
     plt.plot(simulation_seconds, percentage)
     plt.plot(simulation_seconds2, percentage2)
     plt.plot(simulation_seconds3, percentage3)
+    plt.xlabel("Время симуляции, с")
+    plt.ylabel("Процент исследования, %")
     plt.show()
     
     plt.plot(simulation_seconds, average_covered_path)
     plt.plot(simulation_seconds2, average_covered_path2)
     plt.plot(simulation_seconds3, average_covered_path3)
+    plt.xlabel("Время симуляции, с")
+    plt.ylabel("Средний пройденный путь, м")
     plt.show()
     
     
--- src/launcher_packages/simulation_scenarios/launch/grs_3_robot_group_exploration.launch.py
+++ src/launcher_packages/simulation_scenarios/launch/grs_3_robot_group_exploration.launch.py
@@ -1,5 +1,11 @@
-from launch import LaunchDescription
+import os
 import launch_ros.actions as actions
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -17,8 +23,8 @@ def generate_launch_description():
     ]
     
     
-    # map_name = 'map_test.tmx'
-    map_name = 'corridor_maze.tmx'
+    map_name = 'map_test.tmx'
+    # map_name = 'corridor_maze.tmx'
     
     # 1. Симулятор
     
@@ -41,9 +47,9 @@ def generate_launch_description():
             ld.add_action(actions.Node(
                 package="augv_regulators",
                 executable="omni_regulator_node",
-                namespace=f"robot{i + 1}",
+                namespace=f"robot{i}",
                 parameters=[
-                    {'id': i + 1},
+                    {'id': i},
                 ],
                 # emulate_tty=True, output='screen'
             ))
@@ -51,9 +57,9 @@ def generate_launch_description():
             ld.add_action(actions.Node(
                 package="augv_regulators",
                 executable="ackerman_regulator_node",
-                namespace=f"robot{i + 1}",
+                namespace=f"robot{i}",
                 parameters=[
-                    {'id': i + 1},
+                    {'id': i},
                 ],
                 # emulate_tty=True, output='screen'
             ))
@@ -61,9 +67,9 @@ def generate_launch_description():
             ld.add_action(actions.Node(
                 package="augv_regulators",
                 executable="tracked_regulator_node",
-                namespace=f"robot{i + 1}",
+                namespace=f"robot{i}",
                 parameters=[
-                    {'id': i + 1},
+                    {'id': i},
                 ],
                 # emulate_tty=True, output='screen'
             ))
@@ -75,9 +81,9 @@ def generate_launch_description():
                 actions.Node(
                 package="potential_planner", 
                 executable="potential_fields",
-                namespace=f"robot{i + 1}",
+                namespace=f"robot{i}",
                 parameters=[
-                    {'id': i + 1},
+                    {'id': i},
                 ],
                 # emulate_tty=True,
                 # output="screen"
@@ -89,9 +95,9 @@ def generate_launch_description():
                 actions.Node(
                 package="astar_planner", 
                 executable="astar",
-                namespace=f"robot{i + 1}",
+                namespace=f"robot{i}",
                 parameters=[
-                    {'id': i + 1},
+                    {'id': i},
                 ],
                 # emulate_tty=True,
                 # output="screen"
@@ -103,9 +109,9 @@ def generate_launch_description():
                 actions.Node(
                 package="autopilot_lite", 
                 executable="autopilot_lite_node",
-                namespace=f"robot{i + 1}",
+                namespace=f"robot{i}",
                 parameters=[
-                    {'id': i + 1},
+                    {'id': i},
                 ],
                 # emulate_tty=True,
                 # output="screen"
@@ -117,9 +123,9 @@ def generate_launch_description():
                 actions.Node(
                 package="robot_info", 
                 executable="robot_info_node",
-                namespace=f"robot{i + 1}",
+                namespace=f"robot{i}",
                 parameters=[{
-                    'id': i + 1,
+                    'id': i,
                     'platform_type': robot_types[i],
                 }],
                 # emulate_tty=True,
@@ -132,13 +138,42 @@ def generate_launch_description():
                 actions.Node(
                 package="group_exploration", 
                 executable="group_exploration_node",
-                namespace=f"robot{i + 1}",
+                namespace=f"robot{i}",
                 parameters=[{
-                    'id': i + 1,
+                    'id': i,
                 }],
                 emulate_tty=True,
                 output="screen"
             )
         )
+
+        args = {
+            'robot_id': f'{i}',
+            'max_nb_robots': '3',
+            'cslam_config_file': 'sim_lidar.yaml'
+            }.items()
+            
+
+        launch_action = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+         get_package_share_directory('cslam_experiments'), 'launch/robot_experiments'),
+         '/experiment_lidar_sim.launch.py']), launch_arguments=args
+        )
+
+        ld.add_action(launch_action)
+
+        # # 7. exploration для каждого робота
+        # ld.add_action(
+        #         actions.Node(
+        #         package="group_exploration", 
+        #         executable="group_exploration_node",
+        #         namespace=f"robot{i}",
+        #         parameters=[{
+        #             'id': i,
+        #         }],
+        #         emulate_tty=True,
+        #         output="screen"
+        #     )
+        # )
 
     return ld
